6399feb072e0f9aa4c3aa9511c5b216abe122578
jdelacruzv <jldlcv@gmail.com>
2021-02-26 12:17:10 -0500

    Changes in class names

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 /* Variables app reloj */ 
-let white_lines = document.getElementById("white-lines")
-let hours = document.getElementById("clock-hour")
-let minutes = document.getElementById("clock-minute")
-let seconds = document.getElementById("clock-second")
+let gray_lines = document.getElementById("lines")
+let hours = document.getElementById("hour")
+let minutes = document.getElementById("minute")
+let seconds = document.getElementById("second")
 let about = document.getElementById("about")
 const MAX_LINE = 60
 
@@ -10,20 +10,23 @@ const MAX_LINE = 60
 windowAbout = () => swal("José De La Cruz", "Desarrollador Javascript Frontend", "info")
 about.addEventListener("click", windowAbout)
 
-/* Crea 60 líneas blancas dentro del reloj dando la forma de un circulo (360 grados) */
+/* Crea 60 líneas grises dentro del reloj dando la forma de un circulo (360 grados) */
 createWhiteLines = () => {
-	for(var i=1; i<=MAX_LINE; i++) {
+	for(let i=1; i<=MAX_LINE; i++) {
 		// Crea un elemento del DOM con el espacio de nombres URI y el tipo de elemento, en este caso una línea
-		var line = document.createElementNS("http://www.w3.org/2000/svg", "line")
+		let line = document.createElementNS("http://www.w3.org/2000/svg", "line")
 		line.setAttribute("id", `white-line-${i}`)
-		line.setAttribute("class", i % 5 === 0 ? "thick-line" : "thin-line")
+		line.setAttribute("class", i % 5 === 0 ? "clock__lines--thick" : "clock__lines--thin")
 		line.setAttribute("x1", "100")
 		line.setAttribute("y1", "30")
 		line.setAttribute("x2", "100")
 		line.setAttribute("y2", i % 5 === 0 ? "40" : "35")
+
 		// Tranforma la posición del contenido rotando la posición cada 6 grados
 		line.setAttribute("transform", `rotate(${6*i}, 100, 100)`)
-		white_lines.append(line)
+
+		// Agrega las líneas grises al reloj 
+		gray_lines.append(line)
 	}
 }
 
@@ -35,6 +38,7 @@ showTime = () => {
 	let mm = time.getMinutes() * 6		// 60mm * 6° = 360° 
 	let ss = time.getSeconds() * 6		// 60ss * 6° = 360°
 
+	// Establece los atributos a las horas, minutos y segundos
 	hours.setAttribute('transform', `rotate(${hh.toFixed(2)}, 100, 100)`)
 	minutes.setAttribute('transform', `rotate(${mm.toFixed(2)}, 100, 100)`)
 	seconds.setAttribute('transform', `rotate(${ss.toFixed(2)}, 100, 100)`)
@@ -42,4 +46,5 @@ showTime = () => {
 
 /* Llama a las funciones para iniciar el reloj */
 createWhiteLines()
-setInterval(showTime, 1000)
+// setInterval(showTime, 1000)
+setInterval(showTime, 0)
